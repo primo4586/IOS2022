f23b599ef5768051fb378dfab904f1e15eb468d2
Yokach <yokach@walla.com>
2022-09-30 12:12:57 +0300

    added joystick and created axess configurations
    default commad - arcadeDrive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,16 @@
 
 package frc.robot;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.ManualRotateChain;
 import frc.robot.subsystems.Climb;
+import frc.robot.subsystems.Driver;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -16,12 +22,22 @@ import frc.robot.subsystems.Climb;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+  // joystick setup
+  private Joystick d_Controller;
 
+  // subsystems setup
   private Climb climb;
+  private Driver drive;
+
+ 
+  
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer(Climb climb) {
+  public RobotContainer(Climb climb, Driver drive) {
     this.climb = climb;
+    this.drive = drive;
+    this.d_Controller = new Joystick(0);
+    
 
     buildDefaultCommands();
     // Configure the button bindings
@@ -35,7 +51,8 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    
+    // driveTrain controls
+    drive.setDefaultCommand(new ArcadeDrive(drive, () -> d_Controller.getRawAxis(XboxController.Axis.kLeftY.value), () -> d_Controller.getRawAxis(XboxController.Axis.kRightX.value)));
   }
 
   public void buildDefaultCommands() {
